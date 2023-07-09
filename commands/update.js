9be30a12d988393b4d079660e0a0058e70219197
Kringinal <53836468+Kringinal@users.noreply.github.com>
2023-07-09 17:04:51 -0400

    Add files via upload

---
+++ commands/update.js
@@ -0,0 +1,80 @@
+const config = require('../config.json');
+
+const { ActionRowBuilder, ButtonBuilder, SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const axios  = require('axios').default;
+const firebase = require('firebase-admin');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('update')
+    .setDescription('Updates a users roles'),
+
+
+    async execute(interaction) {
+        const member = interaction.member.user.id
+
+        //const OrionRole = interaction.guild.roles.cache.get(config.OrionRole);
+
+        var Profiles = await axios.get(`${config.firebaseURL}Profiles.json`)
+        var Profile
+        for (var pfl in Profiles.data) {
+
+            if (Profiles.data[pfl].DiscordId == member){
+                //console.log(Profiles.data[pfl])
+                Profile = Profiles.data[pfl]
+            }
+        }
+
+        const OrionRole = interaction.guild.roles.cache.find(r => r.name === config.OrionRole);
+        const GuestRole = interaction.guild.roles.cache.find(r => r.name === config.GuestRole); 
+
+        if (Profile && Profile.RobloxId !== 0) {
+            var MessageResponse = interaction.reply({ content: `Processing Command...`});
+            var bodys = await axios.get(`https://users.roblox.com/v1/users/${Profile.RobloxId}`)
+            var response = await axios.get(`https://groups.roblox.com/v2/users/${Profile.RobloxId}/groups/roles`)
+
+            const page6 = new EmbedBuilder()
+                    .setTitle('UNAUTHORIZED')
+                    .setDescription(`I am not able to update your roles/nickname!`)
+                    .setColor(config.ErrorColor)
+                    .setThumbnail(config.GroupLogo);
+
+            if (response.data.data.find(x => x.group.id === 14765837)){
+                try {
+                    await interaction.member.roles.add(OrionRole)
+                    await interaction.member.roles.remove(GuestRole)
+                } catch {
+                    return interaction.editReply({content: "COULDNT GIVE ROLES", embeds: [page6], components: [] });
+                }
+            } else {
+                await interaction.member.roles.remove(OrionRole)
+                await interaction.member.roles.add(GuestRole)
+            }
+
+            var displayName = bodys.data.name
+
+            try {
+                await interaction.member.setNickname(`${displayName}`)
+            } catch(err) {
+                return interaction.editReply({content: "COULDNT CHANGE NICKNAME", embeds: [page6], components: [] });
+            };
+            
+            const REmbed = new EmbedBuilder()
+                .setTitle('VERIFICATION')
+                .setDescription(`Successfully updated your roles!`)
+                .setColor(0x5d65f3)
+                .setThumbnail(config.GroupLogo);
+
+            return interaction.editReply({content: "", embeds: [REmbed] });
+        } else {
+            const EEmbed = new EmbedBuilder()
+                .setTitle('ERROR')
+                .setDescription(`You're not verified! Use /verify to link your ROBLOX account.`)
+                .setColor(config.ErrorColor)
+                .setThumbnail(config.GroupLogo);
+
+            return interaction.editReply({content: "", embeds: [EEmbed]})
+        }
+    },
+};
---
+++ commands/verify.js
@@ -0,0 +1,166 @@
+const config = require('../config.json');
+
+const { ActionRowBuilder, ButtonBuilder, SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const axios  = require('axios').default;
+const firebase = require('firebase-admin');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('verify')
+    .setDescription('Verifies a user with their ROBLOX account.')
+    .addStringOption(option =>
+        option.setName('username')
+            .setDescription('Your ROBLOX username.')
+            .setRequired(true)
+    ),
+
+    async execute(interaction) {
+        const { options } = interaction
+        const member = interaction.member.user.id
+        const username = options.getString('username');
+
+        const OrionRole = interaction.guild.roles.cache.find(role => role.id === config.OrionRole);
+        const GuestRole = interaction.guild.roles.cache.find(role => role.id === config.GuestRole);
+
+        var UserResponse = await axios.post(`https://users.roblox.com/v1/usernames/users`, {
+            "usernames": [username],
+            "excludeBannedUsers": true,
+        })
+        var Data = UserResponse.data.data[0]
+
+        var InitProfile = await axios.get(`${config.firebaseURL}Profiles/${Data.id}_Info.json`)
+
+        if (InitProfile.data !== null && InitProfile.data.RobloxId !== 0) {
+            
+            const newEmbed = new EmbedBuilder()
+                .setTitle('ERROR')
+                .setDescription("You've already verified!")
+                .setColor(config.ErrorColor)
+                .setThumbnail(config.GroupLogo);
+
+            return interaction.reply({ embeds: [newEmbed] });
+        } else {
+            var UserId = null
+
+            if (Data !== null){
+                UserId = Data.id
+            }
+
+            var PendingResponse = await axios.get(`${config.firebaseURL}Pending/${UserId}.json`)
+
+            if (PendingResponse.data && UserId !== 0) {
+                return interaction.reply({ content: 'You can not have more than one verification prompt active at a time!' });
+            } else {
+                var data = await axios.get(`https://users.roblox.com/v1/users/${UserId}`)
+                console.log(UserId, data.data, data.data.name)
+                if (!data.data) {
+                    return interaction.reply({ content: `Could not find the username ${username}. Please check your spelling and try again.` });
+                }
+                var displayName = data.data.name
+                const page1 = new EmbedBuilder()
+                    .setTitle('VERIFICATION')
+                    .setDescription(`To verify account ownership, join the [verification game](${config.verificationLink}).`)
+                    .setColor(0x5d65f3)
+                    .setThumbnail(config.GroupLogo);
+                const page2 = new EmbedBuilder()
+                    .setTitle('VERIFICATION')
+                    .setDescription(`Sorry, but it looks like you haven't completed the steps in the [verification game](${config.verificationLink}). Please complete the steps then click "DONE".`)
+                    .setColor(config.ErrorColor)
+                    .setThumbnail(config.GroupLogo);
+                const page3 = new EmbedBuilder()
+                    .setTitle('VERIFICATION')
+                    .setDescription(`Account ownership verified! Welcome to Orion Core, ${displayName}.`)
+                    .setColor(0x5d65f3)
+                    .setThumbnail(config.GroupLogo);
+                const page4 = new EmbedBuilder()
+                    .setTitle('VERIFICATION')
+                    .setDescription(`Account verification cancelled. To try again, run /verify.`)
+                    .setColor(0x5d65f3)
+                    .setThumbnail(config.GroupLogo);
+                const page5 = new EmbedBuilder()
+                    .setTitle('TIMEOUT')
+                    .setDescription(`Verification timed out. To try again, run /verify.`)
+                    .setColor(0x5d65f3)
+                    .setThumbnail(config.GroupLogo);
+                const page6 = new EmbedBuilder()
+                    .setTitle('UNAUTHORIZED')
+                    .setDescription(`I am not able to update your roles/nickname!`)
+                    .setColor(config.ErrorColor)
+                    .setThumbnail(config.GroupLogo);
+                const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('done')
+                        .setLabel('DONE')
+                        .setStyle(1),
+                    new ButtonBuilder()
+                        .setCustomId('cancel')
+                        .setLabel('CANCEL')
+                        .setStyle(4)
+                );
+
+                firebase.database().ref(`Pending/${UserId}`).set(member)
+
+                interaction.reply({ embeds: [page1], components: [row]  }).then(message => {
+                    const filter = i => i.user.id === interaction.user.id;
+                    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 300000 });
+
+                    collector.on('collect', async i => {
+                        console.log(i.customId)
+                        i.deferUpdate();
+                        if (i.customId === 'done') {
+                           // const AwaitingMessage = aw
+                            var LastProfile = await axios.get(`${config.firebaseURL}Profiles/${UserId}_Info.json`)
+
+                            if (LastProfile.data !== null && LastProfile.data.RobloxId !== 0) {
+                                console.log(LastProfile.data, LastProfile.data.RobloxId)
+                                var GroupResponse = await axios.get(`https://groups.roblox.com/v2/users/${UserId}/groups/roles`)
+
+                                if (GroupResponse.data.data.find(x => x.group.id === 14765837)){
+                                    try {
+                                        await interaction.member.roles.add(OrionRole)
+                                        await interaction.member.roles.remove(GuestRole)
+                                    } catch {
+                                        return interaction.editReply({ embeds: [page6], components: [] });
+                                    }
+                                } else {
+                                    await interaction.member.roles.remove(OrionRole)
+                                    await interaction.member.roles.add(GuestRole)
+                                }
+
+                                try {
+                                    await interaction.member.setNickname(`${displayName}`)
+                                } catch(err) {
+                                    return interaction.editReply({ embeds: [page6], components: [] });
+                                };
+
+                                return interaction.editReply({ embeds: [page3], components: [] });
+                            } else {
+                                return interaction.editReply({ embeds: [page2], components: [row] });
+                            };
+                        } else if (i.customId === 'cancel') {
+                            var LastPending = axios.get(`${config.firebaseURL}Pending/${UserId}.json`)
+
+                            if (LastPending.data !== null) {
+                                firebase.database().ref(`Pending/${UserId}`).set({})
+                            }
+
+                            return interaction.editReply({ embeds: [page4], components: [] });
+                        };
+                    })
+
+                    collector.on('end', async i => {
+                        var LastPending = await axios.get(`${config.firebaseURL}Pending/${UserId}.json`)
+
+                        if (LastPending.data !== null) {
+                            firebase.database().ref(`Pending/${UserId}`).set({})
+                        }
+
+                        return interaction.editReply({ embeds: [page5], components: [] });
+                    });
+                });
+            }
+        }
+    },
+};
