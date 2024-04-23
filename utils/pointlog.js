2c124da6b1b6c02a478290c95b9441bc8bfc63dc
Kringinal <53836468+Kringinal@users.noreply.github.com>
2024-04-23 03:03:44 -0400

    Update pointlog.js

--- utils/pointlog.js
+++ utils/pointlog.js
@@ -10,41 +10,31 @@ module.exports = {
   logpromotion: async function (TargetUserId, OldRank, NewRank) {
     const { data } = await axios.get(`http://users.roblox.com/v1/users/${TargetUserId}`);
     var Avatar = await axios.get(`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${TargetUserId}&size=420x420&format=Png&isCircular=true`)
+    
+   const embed = new EmbedBuilder()
+    .setTitle(`**PROMOTION**`)
+    .setDescription(`[${data.name}](https://www.roblox.com/users/${TargetUserId}/profile) has been promoted! \n \n PREV: **${OldRank} ** \n NEW: **${NewRank}**`)
+    .setTimestamp()
+    .setColor(`#8CFF00`)       
+    .setThumbnail(Avatar.data.data[0].imageUrl)
 
-    try {
-     const embed = new EmbedBuilder()
-      .setTitle(`**PROMOTION**`)
-      .setDescription(`[${data.name}](https://www.roblox.com/users/${TargetUserId}/profile) has been promoted! \n \n PREV: **${OldRank} ** \n NEW: **${NewRank}**`)
-      .setTimestamp()
-      .setColor(`#8CFF00`)       
-      .setThumbnail(Avatar.data.data[0].imageUrl)
-
-       const channel = client.channels.cache.find(ch => ch.name == "🔰┊promotions")
-       return channel.send({ embeds: [embed] })
-    } catch (er) {
-      console.log(er)
-    }
+     const channel = client.channels.cache.find(ch => ch.name == "🔰┊promotions")
+     return channel.send({ embeds: [embed] })
   },
 
   logAccpetance: async function (TargetUserId) {
     const { data } = await axios.get(`http://users.roblox.com/v1/users/${TargetUserId}`);
-     var Avatar = await axios.get(`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${TargetUserId}&size=420x420&format=Png&isCircular=true`)
+    var Avatar = await axios.get(`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${TargetUserId}&size=420x420&format=Png&isCircular=true`)
 
+    const embed = new EmbedBuilder()
+    .setTitle(`**ACCEPTANCE**`)
+    .setDescription(`[${data.name}](https://www.roblox.com/users/${TargetUserId}/profile) has been accepted into **Orion Core**! \n \n **ACCEPTANCE OBBY COMPLETED**`)
+    .setTimestamp()
+    .setColor(`#8CFF00`)       
+    .setThumbnail(Avatar.data.data[0].imageUrl)
 
-    try {
-
-     const embed = new EmbedBuilder()
-      .setTitle(`**ACCEPTANCE**`)
-      .setDescription(`[${data.name}](https://www.roblox.com/users/${TargetUserId}/profile) has been accepted into **Orion Core**! \n \n **ACCEPTANCE OBBY COMPLETED**`)
-      .setTimestamp()
-      .setColor(`#8CFF00`)       
-      .setThumbnail(Avatar.data.data[0].imageUrl)
-
-       const channel = client.channels.cache.find(ch => ch.name == "🔰┊promotions")
-       return channel.send({ embeds: [embed] })
-    } catch (er) {
-      console.log(er)
-    }
+     const channel = client.channels.cache.find(ch => ch.name == "🔰┊promotions")
+     return channel.send({ embeds: [embed] })
   },
 
   sendmessage: async function (ChannelId, embeds) {
