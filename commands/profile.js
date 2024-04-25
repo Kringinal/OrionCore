6ad4b434e7609424be45ddaa6e883ded8e0f07d1
Kringinal <53836468+Kringinal@users.noreply.github.com>
2024-04-25 03:22:55 -0400

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -72,20 +72,75 @@ module.exports = {
               }
         }
 
-         if (Profiles.data[UserId + "_Info"]) {
-           /* const UserNameResponse = await axios.get(`https://users.roblox.com/v1/users/` + UserId)
-            var CurrentEditingProfile = Profiles.data[UserId + "_Info"]
-            var logs = CurrentEditingProfile.Logs
-           
-            let StartEmbed = new EmbedBuilder()
-              .setTitle(`${UserNameResponse.data.name}'s Logs (${currentlog}/${maxLogs})`)
-              .setDescription(`EVENT TYPE: **${StartingLogInfo.Type}** \nHOST: **${StartingHostInfo.data.name}** \nAMOUNT: **${StartingLogInfo.Marks}**`)
-              .setColor(0x5d65f3)
-              .setThumbnail(StartingHostAvatar.data.data[0].imageUrl)
-              .setTimestamp(StartingLogInfo.DateTime)
+        if (Profiles.data[UserId + "_Info"]) {
+	    const UserProfile = Profile.data[UserId + "_Info"]
+	    const UserNameResponse = await axios.get(`https://users.roblox.com/v1/users/` + UserId)
+	    const OldRank = await rblxFunctions.getRankInGroup(14765837, UserId)
+	    const GroupRanks = await rblxFunctions.getRoles(14765837)
+	    var OldRankName = "TEST"
+	    var OldRankId = 0
+	    var NextRank = "TEST"
+	    var NewRankId = 0
+	    
+	    for (let i=0; i < GroupRanks.length; i++) {
+		if (GroupRanks[i].rank == OldRank || GroupRanks[i].id == OldRank){
+		  if (GroupRanks[i+1] == null){
+		    NextRank = "Builderman"
+		    NewRankId = i
+		    OldRankName = GroupRanks[i].name
+		    OldRankId = i
+		  } else {
+		    NextRank = GroupRanks[i+1].name
+		    NewRankId = i+1
+		    OldRankName = GroupRanks[i].name
+		    OldRankId = i
+		  }
+		}
+	    }
 
-            interaction.reply({ embeds: [StartEmbed]})
-           */
+	    var Requirements = await axios.get(`${config.firebaseURL}Requirements.json`)
+	    var NextAmount = 0
+
+	    if (Requirements[NewRankId] !== null) {
+		NextAmount = Requirements[NewRankId]
+	    }
+
+	    var Percentage = UserProfile.Marks / NextAmount
+	    var PercentBar = ":black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+
+	    if (Percentage >= .1 && Percentage < .2) {
+		PercentBar = ":green_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	    } else if (Percentage >= .2 && Percentage < .3) {
+		PercentBar = ":green_square: :green_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	    } else if (Percentage >= .3 && Percentage < .4) {
+		PercentBar = ":green_square: :green_square: :green_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	    } else if (Percentage >= .4 && Percentage < .5) {
+		PercentBar = ":green_square: :green_square: :green_square: :green_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	    } else if (Percentage >= .5 && Percentage < .6) {
+		PercentBar = ":green_square: :green_square: :green_square: :green_square: :green_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	    } else if (Percentage >= .6 && Percentage < .7) {
+		PercentBar = ":green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	    } else if (Percentage >= .7 && Percentage < .8) {
+		PercentBar = ":green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :black_large_square: :black_large_square: :black_large_square:"
+	    } else if (Percentage >= .8 && Percentage < .9) {
+		PercentBar = ":green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :black_large_square: :black_large_square:"
+	    } else if (Percentage >= .9 && Percentage < 1) {
+		PercentBar = ":green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :black_large_square:"
+	    } else if (Percentage >= 1) {
+		PercentBar = ":green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square:"
+	    }
+
+	    var Avatar = await axios.get(`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${UserId}&size=420x420&format=Png&isCircular=true`)
+
+	    let StartEmbed = new EmbedBuilder()
+              .setTitle(`${UserNameResponse.data.name}`)
+	      .setUrl(`https://www.roblox.com/users/${UserId}/profile`)
+              .setDescription(`${PercentBar} **${Percentage * 100}%** \n \nRank: **${OldRank}** \nMarks: **${UserProfile.Marks}** \n \n**${NextAmount - UserProfile.Marks}** Marks remaining for **${NextRank} (${NextAmount} Marks)**`)
+              .setColor(0x5d65f3)
+              .setThumbnail(Avatar.data.data[0].imageUrl)
+              .setTimestamp()
+
+            return interaction.reply({ embeds: [StartEmbed]})
         }   
     }
 }
