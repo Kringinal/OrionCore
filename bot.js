59490774108f5bbaf5093d8456a2eaf4378d61ea
Zack <sneakeliterblx@gmail.com>
2025-06-04 18:38:12 -0400

    fixed

--- bot.js
+++ bot.js
@@ -6,13 +6,11 @@ const config = require('./config.json')
 const firebase = require('firebase-admin')
 const cron = require('node-cron');
 const app = express();
-const rblxFunctions = require("noblox.js");
 const serviceAccount = require('./orion-core-61636-firebase-adminsdk-4c2ck-a996d6a61a.json')
 const firebaseConfig = {
     credential: firebase.credential.cert(serviceAccount),
     databaseURL: `${config.firebaseURL}`,
 };
-var loggedIn = false
 firebase.initializeApp(firebaseConfig)
 
 
@@ -71,9 +69,6 @@ discordClient.on('interactionCreate', async (interaction) => {
 
 let index = require('./routes/index');
 let api = require('./routes/api');
-let Logger = require('./utils/pointlog.js')
-
-Logger.registerclient(discordClient)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -84,25 +79,6 @@ app.use('/api', api);
 app.listen(process.env.PORT || 5000, () => {
   console.log('listening');
 });
-
-async function rblx_login() {
-    var Cookie = await axios.get(`${config.firebaseURL}CookieInfo.json`)
-    console.log("cookie Data" + Cookie.data.Cookie)
-    if (Cookie.data) {
-        rblxFunctions.setAPIKey("McNl+Q5N0U2QDGIFSmB3wEjR+sYplnEdqVvrbfp14EsQWKsUZXlKaGJHY2lPaUpTVXpJMU5pSXNJbXRwWkNJNkluTnBaeTB5TURJeExUQTNMVEV6VkRFNE9qVXhPalE1V2lJc0luUjVjQ0k2SWtwWFZDSjkuZXlKaVlYTmxRWEJwUzJWNUlqb2lUV05PYkN0Uk5VNHdWVEpSUkVkSlJsTnRRak4zUldwU0szTlpjR3h1UldSeFZuWnlZbVp3TVRSRmMxRlhTM05WSWl3aWIzZHVaWEpKWkNJNklqSTRORE13T0RraUxDSmhkV1FpT2lKU2IySnNiM2hKYm5SbGNtNWhiQ0lzSW1semN5STZJa05zYjNWa1FYVjBhR1Z1ZEdsallYUnBiMjVUWlhKMmFXTmxJaXdpWlhod0lqb3hOek13T1RJMU56UXpMQ0pwWVhRaU9qRTNNekE1TWpJeE5ETXNJbTVpWmlJNk1UY3pNRGt5TWpFME0zMC5YRV9LWEJoMkVhS0hfMUVCa05OREk4QndaLUEtR3NLcUcxdkZ2N0xkOHMzeEZXNDZqb3A3NDR1LXFHalNIZTRuMnlvQ3YzcjAxR3hPY3JIeW1WZ3hxYjJoNkk4amVkWmpzVjQ1ZkNnYk9QNlFXSWc5Wmx6NmFFZ2UtY3FhUUt3NzlYa21xemRIX1dwVjVBSmRLR2J6WG40ZGJzMXl2V3drSExRaVpvNU9nanJwZWxyUnZWdzQyT2F2MllCYkdkYV9sRmZBSXJ4WWNSb05DUkNuakRhTmc4cHN5WjhBM2lrb2k2dzB4dHoxUUJrX0pfRm5uTnhDRHJETFFVT3ZWVmtIc2s0TldHWGxRaGVkdWpXZkcxS3p4TGtVUWE4MEJ3bElEdlhNSHM3aFJkTDdISy1kNDRQZkxLaHFzNURTXzF6WW9MSjdBUF9NYWJuanpwVm40YTRIRGc=")
-    }
-  }
-
-cron.schedule('*/30 * * * *', () => {
-    if (loggedIn == true) {
-      loggedIn = false
-      rblxFunctions.refreshCookie().then(function(newCookie) {
-          firebase.database().ref(`CookieInfo/Cookie`).set(newCookie)
-        rblx_login();
-        console.log("Cookie refreshed, validated and relogged in.")
-      })
-    }
-  });
 
 cron.schedule('* * * * *', () => {
   axios.get('https://orioncore-3b6068b75ef5.herokuapp.com/');
--- commands/marks.js
+++ commands/marks.js
@@ -3,35 +3,40 @@ const config = require('../config.json');
 const { ActionRowBuilder, ButtonBuilder, SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 const axios  = require('axios').default;
-const rblxFunctions = require("noblox.js");
 const firebase = require('firebase-admin');
 let db = firebase.database();
 
-const logPromotion = require('../utils/pointlog.js');
-
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('marks')
-    .setDescription('add/remove provided username(s) Marks.')
+    .setName('banlist')
+    .setDescription('add/remove from the Arvorian Banlist.')
     .addStringOption((option) =>
         option.setName('addorremove')
-	    .setDescription('Choose whether you want to add/remove Marks.')
+	    .setDescription('Choose whether you want to add/remove from the banlist.')
 	    .setRequired(true)
 	    .addChoices(
 		{ name: 'Add', value: 'add' },
 		{ name: 'Remove', value: 'remove' },
 	    )
     )
-    .addNumberOption((option) =>
-        option.setName('amount')
-	    .setDescription('The amount of Marks you want to give/remove.')
-            .setRequired(true)
-            .setMinValue(1)
-            .setMaxValue(7)
+    .addStringOption((option) =>
+        option.setName('type')
+	    .setDescription('Group or user')
+        .setRequired(true)
+        .addChoices(
+        { name: 'Group', value: 'group' },
+        { name: 'User', value: 'user' },
+        )
     )
     .addStringOption((option) =>
-        option.setName('usernames')
-            .setDescription('Enter the username(s) of those you would like to be affected.')
+        option.setName('id')
+            .setDescription('Enter the userid or groupid.')
+            .setRequired(true)
+    )
+
+    .addStringOption((option) =>
+        option.setName('reason')
+            .setDescription('Enter the reason for the ban.')
             .setRequired(true)
     ),
 
@@ -39,137 +44,99 @@ module.exports = {
     async execute(interaction) {
         var HostUserID = interaction.member.user.id
 
-	var MessageResponse = interaction.reply({ content: `Processing Command...`});
+	    var MessageResponse = interaction.reply({ content: `Processing Command...`});
 	    
         const { options } = interaction
 
         const addremovechoice = options.getString('addorremove');
-        var amount = options.getNumber('amount')
-        const usernames = options.getString('usernames')
+        var amount = options.getString('type')
+        const id = options.getString('id')
+        const reason = options.getString('reason')
 
-	if (addremovechoice == 'remove') {
-	    amount = -amount
-	}
+        if (addremovechoice == 'remove' && type == 'user') {
+            var Profile = await axios.get(`${config.firebaseURL}User_Banlist/${id}_User`)
 
-        const usernamebreakdown = usernames.split(" ")
-	console.log(usernamebreakdown.length)
+            if (Profile !== null){
+               await firebase.database().ref(`User_Banlist/${id}_User`).set({})
+
+                const EEmbed = new EmbedBuilder()
+                    .setTitle('SUCCESS')
+                    .setDescription(`${id} HAS BEEN REMOVED FROM THE BANLIST`)
+                    .setColor(0x5d65f3)
+                    .setTimestamp()
+		        interaction.channel.send({content: "", embeds: [EEmbed]})
+            } else {
+                const EEmbed = new EmbedBuilder()
+                    .setTitle('ERROR')
+                    .setDescription(`${id} IS NOT ON THE BANLIST`)
+                    .setColor(config.ErrorColor)
+                    .setTimestamp()
+		        interaction.channel.send({content: "", embeds: [EEmbed]})
+            }
 	    
-        var Profiles = await axios.get(`${config.firebaseURL}Profiles.json`)
+        } else if (addremovechoice == 'add' && type == 'user') {
+             var Profile = await axios.get(`${config.firebaseURL}User_Banlist/${id}_User`)
 
-         for (var pfl in Profiles.data) {
-            if (Profiles.data[pfl].DiscordId == HostUserID){
-                HostUserID = Profiles.data[pfl].RobloxId
+            if (Profile == null){
+                await firebase.database().ref(`User_Banlist/${id}_User`).set({Reason: reason})
+
+                const EEmbed = new EmbedBuilder()
+                    .setTitle('SUCCESS')
+                    .setDescription(`${id} HAS BEEN ADDED TO THE BANLIST`)
+                    .setColor(0x5d65f3)
+                    .setTimestamp()
+		        interaction.channel.send({content: "", embeds: [EEmbed]})
+            } else {
+                const EEmbed = new EmbedBuilder()
+                    .setTitle('ERROR')
+                    .setDescription(`${id} IS ALREADY ON THE BANLIST`)
+                    .setColor(config.ErrorColor)
+                    .setTimestamp()
+		        interaction.channel.send({content: "", embeds: [EEmbed]})
             }
         }
-        
-        for (const currentuser of usernamebreakdown) {
-            var UserId = null
-            
-            if (currentuser.startsWith("<@")) {
-                const RevisedDiscordID = currentuser.toString().replace(/[\\<>@#&!]/g, "")
 
-                for (var pfl in Profiles.data) {
-                    if (Profiles.data[pfl].DiscordId == RevisedDiscordID){
-                        UserId = pfl.toString().replace("_Info", "")
-                    }
-                }
 
-                if (UserId == null) {	
-                    const EEmbed = new EmbedBuilder()
-                        .setTitle('COULD NOT EDIT PROFILE')
-                        .setDescription(`The mentioned user ${currentuser}, is not verified with the Orion Bot.`)
-                        .setColor(config.ErrorColor)
-                        .setThumbnail(config.GroupLogo)
-                        .setTimestamp()
+        if (addremovechoice == 'remove' && type == 'group') {
+             var Profile = await axios.get(`${config.firebaseURL}Group_Banlist/0_List/${id}_Group`)
 
-		    interaction.channel.send({content: "", embeds: [EEmbed]})
-                }
+            if (Profile !== null){
+                await firebase.database().ref(`Group_Banlist/0_List/${id}_Group`).set({})
+
+                const EEmbed = new EmbedBuilder()
+                    .setTitle('SUCCESS')
+                    .setDescription(`${id} HAS BEEN REMOVED FROM THE BANLIST`)
+                    .setColor(0x5d65f3)
+                    .setTimestamp()
+		        interaction.channel.send({content: "", embeds: [EEmbed]})
             } else {
-                var UserResponse = await axios.post(`https://users.roblox.com/v1/usernames/users`, {
-                     "usernames": [currentuser],
-                    "excludeBannedUsers": true,
-                })
-                var Data = UserResponse.data.data[0]
-
-                if (Data !== null){
-                    UserId = Data.id
-                } else {
-                     const EEmbed = new EmbedBuilder()
-                        .setTitle('COULD NOT EDIT PROFILE')
-                        .setDescription(`The provided username ${currentuser}, does not exist on ROBLOX.`)
-                        .setColor(config.ErrorColor)
-                        .setThumbnail(config.GroupLogo)
-                        .setTimestamp()
-
-			interaction.channel.send({content: "", embeds: [EEmbed]})
-                }
+                const EEmbed = new EmbedBuilder()
+                    .setTitle('ERROR')
+                    .setDescription(`${id} IS NOT ON THE BANLIST`)
+                    .setColor(config.ErrorColor)
+                    .setTimestamp()
+		        interaction.channel.send({content: "", embeds: [EEmbed]})
             }
+        } else if (addremovechoice == 'add' && type == 'group') {
+            var Profile = await axios.get(`${config.firebaseURL}Group_Banlist/0_List/${id}_Group`)
 
-            const currentDate = new Date();
-            const timestamp = currentDate.getTime();
-            
-            if (Profiles.data[UserId + "_Info"]) {
-               var CurrentEditingProfile = Profiles.data[UserId + "_Info"]
-                    var marks = CurrentEditingProfile.Marks
-                    var logs = CurrentEditingProfile.Logs
-                    var Lastupdated = CurrentEditingProfile.LastUpdated
-                    var Discordid = CurrentEditingProfile.DiscordId
-		    var Robloxid = CurrentEditingProfile.RobloxId
-            
-                logs[logs.length] = {
-                    DateTime: timestamp,
-                    HostId: HostUserID,
-                    Marks: amount,
-                    Type: "Discord Integration"
-                }
+            if (Profile == null) {
+                 await firebase.database().ref(`Group_Banlist/0_List/${id}_Group`).set({Reason: reason})
 
-               	db.ref(`Profiles/${UserId}_Info/`).set({
-                    Marks: marks + amount,
-                    Logs: logs,
-                    LastUpdated: timestamp,
-                    RobloxId: Robloxid,
-                    DiscordId:  Discordid
-                })
-	   	 var Avatar = await axios.get(`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${Robloxid}&size=420x420&format=Png&isCircular=true`)
-	    	const CurrRank = await rblxFunctions.getRankInGroup(14765837, Robloxid)
-	    	const Requirements = await axios.get(`${config.firebaseURL}Requirements.json`)
-		const UserNameResponse = await axios.get(`https://users.roblox.com/v1/users/` + Robloxid)	
-		    
-	   	 const REmbed = new EmbedBuilder()
-			 .setTitle(`**${UserNameResponse.data.name}**`)
-			.setURL(`https://www.roblox.com/users/${Robloxid}/profile`)
-			.setDescription(`\n \n **${marks}** -> **` + (marks + amount) + `**`)
-			.setColor(0x5d65f3)
-			.setThumbnail(Avatar.data.data[0].imageUrl)
-			.setTimestamp()
-
-		await interaction.channel.send({content: "", embeds: [REmbed]})
-
-		console.log(Requirements.data)
-	      if (Requirements.data[CurrRank+1] !== null) {
-		if (marks + amount >= Requirements.data[CurrRank+1]) {
-		     const UserNameResponse = await axios.get(`https://users.roblox.com/v1/users/` + Robloxid)				     
-		        axios({
-			     method: 'post',
-			     url: `https://orioncore-3b6068b75ef5.herokuapp.com/api/promote`,
-			     headers: {'Content-Type': 'application/json',}, 
-			     data: {
-			       userid: Robloxid,
-			       username: UserNameResponse.data.name,
-			     }
-		        });
-		   }
-	       }
+                 const EEmbed = new EmbedBuilder()
+                    .setTitle('SUCCESS')
+                    .setDescription(`${id} HAS BEEN ADDED TO THE BANLIST`)
+                    .setColor(0x5d65f3)
+                    .setTimestamp()
+		        interaction.channel.send({content: "", embeds: [EEmbed]})
             } else {
-		const EEmbed = new EmbedBuilder()
-		.setTitle('COULD NOT EDIT PROFILE')
-		.setDescription(`The mentioned user ${currentuser}, is not verified with the Orion Bot.`)
-		.setColor(config.ErrorColor)
-		.setThumbnail(config.GroupLogo)
-		.setTimestamp()
-
-	    	interaction.channel.send({content: "", embeds: [EEmbed]})
-	    }
+                const EEmbed = new EmbedBuilder()
+                    .setTitle('ERROR')
+                    .setDescription(`${id} IS ALREADY ON THE BANLIST`)
+                    .setColor(config.ErrorColor)
+                    .setTimestamp()
+		        interaction.channel.send({content: "", embeds: [EEmbed]})
+            }
         }
     },
 };
